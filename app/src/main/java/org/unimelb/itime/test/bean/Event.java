b96f28b233b16cf050df0eb1a6fb170569f00584
yinchuandong <yincd520@sina.com>
2016-08-22 16:49:32 +1000

    fix: complete maven local

---
+++ app/src/main/java/org/unimelb/itime/test/bean/Event.java
@@ -0,0 +1,18 @@
+package org.unimelb.itime.test.bean;
+
+import org.unimelb.itime.vendor.listener.ITimeEventInterface;
+
+/**
+ * Created by yinchuandong on 22/08/2016.
+ */
+public class Event implements ITimeEventInterface{
+    @Override
+    public void setTitle() {
+
+    }
+
+    @Override
+    public void getTitle(String title) {
+
+    }
+}

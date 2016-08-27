e8463987e2bede2cd2f8914ae20308b7ee0dc824
yinchuandong <yincd520@sina.com>
2016-08-27 15:13:42 +1000

    fix: the bug of weekViewFragment.java

--- app/src/main/java/org/unimelb/itime/test/paul/WeekViewFragment.java
+++ app/src/main/java/org/unimelb/itime/test/paul/WeekViewFragment.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import org.unimelb.itime.test.R;
 import org.unimelb.itime.vendor.eventview.Event;
